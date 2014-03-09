d2748c6c416fb022bfa2e66b2f3c18469be4b6d1
kdoyne <kdoyne930@gmail.com>
2014-03-09 19:16:22 -0400

    added circles

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 var body = document.querySelector("body");
-var button = document.querySelector("button");
+var rectangle = document.querySelector("button#rectangle");
+var circle = document.querySelector("button#circle");
+
 
 var count = 0;
 
@@ -16,13 +18,23 @@ var setBackground = function() {
 
 setBackground();
 
-button.addEventListener("click", function() {
+rectangle.addEventListener("click", function() {
   var colorBlock = document.createElement("div");
   body.appendChild(colorBlock);
   colorBlock.id = count;
   colorBlock.setAttribute("style","width:"+setSize()+"px; height:"+setSize()+"px; background-color: "+setColor()+"; position: absolute; top: "+setTopLocation()+"px; left: "+setLeftLocation()+"px;" );
   count++;
 });
+
+circle.addEventListener("click", function(){
+  var colorBlock = document.createElement("div");
+  body.appendChild(colorBlock);
+  colorBlock.id = count;
+  var dimension = setSize()
+  colorBlock.setAttribute("style","width:"+dimension+"px; height:"+dimension+"px; background-color: "+setColor()+"; position: absolute; top: "+setTopLocation()+"px; left: "+setLeftLocation()+"px; border-radius: 50%" );
+  count++;
+});
+
 
 var setTopLocation = function() {
   randomNumber = Math.floor(Math.random()*400);
